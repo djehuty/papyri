ea94a20dc42bc49cf6f6b8c69c46491b9d8f24c5
wilkie <wilkie05@gmail.com>
2011-11-10 14:33:41 -0500

    Adding CLI stubs.

---
+++ lib/papyrus/cli.rb
@@ -0,0 +1,19 @@
+require 'papyrus'
+require 'optparse'
+
+module Papyrus
+  class CLI
+    BANNER = <<-USAGE
+    Usage:
+      papyrus generate
+    USAGE
+
+    class << self
+      def parse_options
+      end
+
+      def run
+      end
+    end
+  end
+end
