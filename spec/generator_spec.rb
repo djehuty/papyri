11a79837d578c05e1dc3346210910f51a6f95eb4
wilkie <wilkie05@gmail.com>
2011-11-10 20:26:54 -0500

    Adding a basic test stub.

---
+++ spec/generator_spec.rb
@@ -0,0 +1,9 @@
+# Generator Specifications
+require 'papyri/generator'
+
+describe Papyri::Generator do
+  describe "#new" do
+    it "generates an HTML file from a YAML description" do
+    end
+  end
+end
