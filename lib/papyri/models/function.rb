862f81b6c5edf46b44b7c9cba8a543443631a362
wilkie <wilkie05@gmail.com>
2011-11-12 14:33:50 -0500

    Adding parameter parsing to function.
    Refactored the prior tests a little.

--- lib/papyri/models/function.rb
+++ lib/papyri/models/function.rb
@@ -1,15 +1,41 @@
+require 'papyri/models/parameter'
+
 module Papyri
   class Function
     attr_reader :name
     attr_reader :description
+    attr_reader :parameters
 
-    def initialize name, description
+    def initialize name, description, params = []
       @name = name
       @description = description
+
+      @parameters = []
+      unless params.nil?
+        params.each do |p|
+          @parameters << Papyri::Parameter.new(p["name"],
+                                               p["description"],
+                                               p["type"])
+        end
+      end
     end
 
     def to_s
-      @name
+      ret = "#{@name} ( "
+
+      @parameters.each do |p|
+        ret << "#{p.type} " unless p.type.nil?
+        ret << "#{p.name}, "
+      end
+
+      if ret.end_with? ", "
+        ret = ret[0..-3]
+        ret << " "
+      end
+
+      ret << ")"
+
+      ret
     end
   end
 end
--- spec/function_spec.rb
+++ spec/function_spec.rb
@@ -3,14 +3,33 @@ require 'papyri/models/function'
 
 describe Papyri::Function do
   describe "#new" do
-    it "generates a class with the specified name" do
-      f = Papyri::Function.new "foobar", "Does a foo bar"
-      f.name.should eql("foobar")
+    before(:each) do
+      @f = Papyri::Function.new "foobar", "Does a foo bar", [
+        {"name"=>"a", "description"=>"does a thing"},
+        {"name"=>"b", "description"=>"does something else"}
+      ]
     end
 
-    it "generates a class with the specified description" do
+    it "generates an instance with the specified name" do
+      @f.name.should eql("foobar")
+    end
+
+    it "generates an instance with the specified description" do
+      @f.description.should eql("Does a foo bar")
+    end
+
+    it "generates instances of Parameter equal to the number of parameters passed" do
+      @f.parameters.length.should eql(2)
+    end
+
+    it "generates an instance with the parameters as an empty array if none are specified" do
       f = Papyri::Function.new "foobar", "Does a foo bar"
-      f.description.should eql("Does a foo bar")
+      f.parameters.should eql([])
+    end
+
+    it "generates an instance with no parameters if an empty array is specified" do
+      f = Papyri::Function.new "foobar", "Does a foo bar", []
+      f.parameters.should eql([])
     end
   end
 end
