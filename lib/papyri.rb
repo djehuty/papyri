9abc085a9579242d46475c2f51da1490e95f6e19
wilkie <wilkie05@gmail.com>
2011-11-14 01:58:57 -0500

    Have it generate directories and Module views.

--- lib/papyri.rb
+++ lib/papyri.rb
@@ -8,8 +8,12 @@ module Papyri
     def generate source_path, dest_path
       puts "Generating..."
 
-      @files = []
       @classes = []
+      @files = []
+      @modules = []
+
+      conf = Papyri::Config.new "#{source_path}/papyri_config.yaml"
+      project = Papyri::Project.new
 
       Dir.new(source_path).each do |f|
         source = "#{source_path}/#{f}"
@@ -17,24 +21,65 @@ module Papyri
           unless f.rindex('.').nil?
             ext = f[f.rindex('.')+1..-1]
             filename = f[0..f.rindex('.')-1]
-            dest = "#{dest_path}/#{filename}"
+            dest = "#{dest_path}"
             unless filename == "papyri_config"
               case ext
               when 'yaml'
-                dest << ".html"
                 cls = Papyri::Class.new(source)
-                Papyri::Generator.new(cls, dest).generate
-                @files << filename.intern
+                mod = cls.module
+
+                # split mod
+                mods = mod.split(conf.module_delimiter)
+                cur_mod = project
+
+                mods[0..-2].each do |m|
+                  found = false
+                  cur_mod.modules.each do |mod|
+                    if mod.name == m
+                      cur_mod = mod
+                      found = true
+                      break
+                    end
+                  end
+
+                  unless found
+                    mod = Papyri::Module.new(m)
+                    @modules << {:model=>mod, :filename=>"#{dest}/#{m}.html"}
+                    cur_mod.add({:model=>mod, :filename=>"#{dest}/#{m}.html"})
+                    cur_mod = mod
+                  end
+
+                  dest << "/#{m}"
+                end
+
+                dest << "/#{filename}.html"
+
                 @classes << {:model=>cls, :filename=>dest}
+                cur_mod.add({:model=>cls, :filename=>dest})
+
+                @files << filename.intern
               end
             end
           end
         end
       end
 
-      nav = Papyri::Project.new(@classes)
-      dest = dest_path + "/navigation.html"
-      Papyri::Generator.new(nav, dest).generate
+      @modules.each do |m|
+        dest = m[:filename]
+        new_path = dest[0..dest.rindex('.')-1]
+        begin
+          Dir.mkdir(new_path)
+        rescue SystemCallError
+        end
+        Papyri::Generator.new(m[:model], dest).generate
+      end
+
+      @classes.each do |c|
+        Papyri::Generator.new(c[:model], c[:filename]).generate
+      end
+
+      dest = dest_path + "/project.html"
+      Papyri::Generator.new(project, dest).generate
     end
   end
 end
