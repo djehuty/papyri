cc8a4177153ea037242cafa2d370aee21d41efd2
wilkie <wilkie05@gmail.com>
2011-11-15 13:10:42 -0500

    Added name and path properties to Theme model.

--- lib/papyri/models/theme.rb
+++ lib/papyri/models/theme.rb
@@ -1,22 +1,26 @@
 module Papyri
   class Theme
+    attr_reader :name
+    attr_reader :path
+
     def initialize theme_name
-      @theme_name = theme_name
-      if not File.exists?("lib/papyri/views/#{@theme_name}/class.haml")
+      @name = theme_name
+      @path = "lib/papyri/views/#{@name}"
+      if not File.exists?("#{@path}/class.haml")
         raise "Theme not found"
       end
     end
 
     def generate_class cls
-      Tilt::HamlTemplate.new("lib/papyri/views/#{@theme_name}/class.haml", 1, {:format => :html5}).render cls
+      Tilt::HamlTemplate.new("#{@path}/class.haml", 1, {:format => :html5}).render cls
     end
 
     def generate_module mod
-      Tilt::HamlTemplate.new("lib/papyri/views/#{@theme_name}/module.haml", 1, {:format => :html5}).render mod
+      Tilt::HamlTemplate.new("#{@path}/module.haml", 1, {:format => :html5}).render mod
     end
 
     def generate_project project
-      Tilt::HamlTemplate.new("lib/papyri/views/#{@theme_name}/project.haml", 1, {:format => :html5}).render project
+      Tilt::HamlTemplate.new("#{@path}/project.haml", 1, {:format => :html5}).render project
     end
   end
 end
--- spec/theme_spec.rb
+++ spec/theme_spec.rb
@@ -12,30 +12,50 @@ describe Papyri::Theme do
       lambda { theme = Papyri::Theme.new "theme_that_does_not_exist" }.should raise_error("Theme not found")
     end
 
+    it "generates an instance that points to the current path" do
+      theme = Papyri::Theme.new "basic"
+      theme.path.should eql("lib/papyri/views/basic")
+    end
+
+    it "generates an instance that can recall its name" do
+      theme = Papyri::Theme.new "basic"
+      theme.name.should eql("basic")
+    end
+
   end
 
   describe "#generate_class" do
     before(:each) do
       @theme = Papyri::Theme.new "basic"
-      @parent = Papyri::Project.new
-      @parent_filename = "project.html"
 
-      @name = "Stream"
-      @module = "io.stream"
-      @constructors = []
-      @methods = []
-      @properties = []
-      @events = []
-      @dependencies = []
+      cls = Class.new do
+        def initialize
+          @parent = Papyri::Project.new
+          @parent_filename = "project.html"
+
+          @name = "Stream"
+          @module = "io.stream"
+          @constructors = []
+          @methods = []
+          @properties = []
+          @events = []
+          @dependencies = []
+        end
+
+        def path_url path
+          path
+        end
+      end
+      @cls = cls.new
     end
 
     it "generates HTML output" do
-      output = @theme.generate_class self
+      output = @theme.generate_class @cls
       output.should match /<HTML.*?>/i
     end
 
     it "generates HTML5 output" do
-      output = @theme.generate_class self
+      output = @theme.generate_class @cls
       output.should match /<!DOCTYPE html>/i
     end
   end
@@ -43,27 +63,38 @@ describe Papyri::Theme do
   describe "#generate_module" do
     before(:each) do
       @theme = Papyri::Theme.new "basic"
-      @parent = Papyri::Project.new
-      @parent_filename = "project.html"
 
-      @classes = []
-      model_a = Papyri::Class.new("test/stream.yaml")
-      @classes << {:model => model_a, 
-        :filename => "stream.html"}
-      model_b = Papyri::Class.new("test/foo.yaml")
-      @classes << {:model => model_b,
-        :filename => "foo.html"}
+      cls = Class.new do
+        def initialize
+          @parent = Papyri::Project.new
+          @parent_filename = "project.html"
 
-      @modules = []
+          @classes = []
+          model_a = Papyri::Class.new("test/stream.yaml")
+          @classes << {:model => model_a, 
+            :filename => "stream.html"}
+          model_b = Papyri::Class.new("test/foo.yaml")
+          @classes << {:model => model_b,
+            :filename => "foo.html"}
+
+          @modules = []
+        end
+
+        def path_url path
+          path
+        end
+      end
+
+      @cls = cls.new
     end
 
     it "generates HTML output" do
-      output = @theme.generate_module self
+      output = @theme.generate_module @cls
       output.should match /<HTML.*?>/i
     end
 
     it "generates HTML5 output" do
-      output = @theme.generate_module self
+      output = @theme.generate_module @cls
       output.should match /<!DOCTYPE html>/i
     end
   end
@@ -72,24 +103,34 @@ describe Papyri::Theme do
     before(:each) do
       @theme = Papyri::Theme.new "basic"
 
-      @classes = []
-      model_a = Papyri::Class.new("test/stream.yaml")
-      @classes << {:model => model_a, 
-        :filename => "stream.html"}
-      model_b = Papyri::Class.new("test/foo.yaml")
-      @classes << {:model => model_b,
-        :filename => "foo.html"}
+      cls = Class.new do
+        def initialize
+          @classes = []
+          model_a = Papyri::Class.new("test/stream.yaml")
+          @classes << {:model => model_a, 
+            :filename => "stream.html"}
+          model_b = Papyri::Class.new("test/foo.yaml")
+          @classes << {:model => model_b,
+            :filename => "foo.html"}
 
-      @modules = []
+          @modules = []
+        end
+
+        def path_url path
+          path
+        end
+      end
+
+      @cls = cls.new
     end
 
     it "generates HTML output" do
-      output = @theme.generate_project self
+      output = @theme.generate_project @cls
       output.should match /<HTML.*?>/i
     end
 
     it "generates HTML5 output" do
-      output = @theme.generate_project self
+      output = @theme.generate_project @cls
       output.should match /<!DOCTYPE html>/i
     end
   end
